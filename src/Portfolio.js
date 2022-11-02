9a3e12e2e51d3e53aa647155cdfeaad095679834
Marcin <marcin.p.dev@gmail.com>
2022-11-02 16:30:32 +0100

    Added portfolio scss

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Portfolio.scss";
 
 class Portfolio extends React.Component {
   render() {
-    return <React.Fragment>Hello world</React.Fragment>;
-  }
+    return (
+    <div id="port">
+        Hello world
+        </div>
+    )  
+}
 }
 
 export default Portfolio;
