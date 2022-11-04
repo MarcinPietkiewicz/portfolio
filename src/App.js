97354bbf8bf691566c1df3c994d1b4f856c03e37
Marcin <marcin.p.dev@gmail.com>
2022-11-04 18:07:24 +0100

    Further styling and icons in about section

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 import React from "react";
 import "./App.scss";
 import Portfolio from "./Portfolio.js";
+import GithubLogo from "./assets/github.svg";
+import LinkedinLogo from "./assets/linkedin.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div id="header-name">Marcin Pietkiewicz</div>
-        <div id="right-menu">
-          <a href="#about">about</a>
-          <a href="#portfolio">portfolio</a>
-          <a href="#contact">contact</a>
+      <header>
+        <div className="header-content">
+          <div id="header-name">Marcin Pietkiewicz</div>
+          <div id="right-menu">
+            <a href="#about">about</a>
+            <a href="#portfolio">portfolio</a>
+            <a href="#contact">contact</a>
+          </div>
         </div>
       </header>
       <main>
         <section id="about">
-          <div className="section-title">about the developer</div>
-          <div className="about-pic-section">
-         
-              <img id="dev-img" src={require("./assets/marcin.jpg")} alt="" />
-            <div className="about-pic">pic</div>
-            <div className="links-section">links logos</div>
-          </div>
-          <div className="about-description">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
-            survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          <div className="section-title white">about the developer</div>
+          <div className="about-container">
+            <div className="about-left">
+              <div className="about-pic-section">
+                <div className="about-pic">
+                  <img id="dev-img" src={require("./assets/marcin.jpg")} alt="" />
+                </div>
+                <div className="about-links-section">
+                  <img id="github" src={GithubLogo} alt="" />
+                  <img id="github" src={LinkedinLogo} alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="about-right">
+              <div className="about-description">
+
+
+                <p>
+                  As a web designer and JavaScript developer, I prioritize the use of clean code, the use of agreed upon syntax conventions,
+                  and a commitment to DRY principles. I’m also devoted to using accessibility and responsiveness in my markup and design.
+                  It’s not enough for me just to make something work. It's important to understand what I have made. Does it work
+                  effectively? Can I easily apply this process to a future project? Have I considered every scenario?
+                </p>
+
+                <p>
+                  Making my contributions count motivates me more than anything. Other than that, I spend my spare time building upon my
+                  development skills and creating my own personal projects. I also enjoy watching PBS documentaries and doing lots of
+                  reading, whether it be nonfiction, novels, memoirs, or comic books. Additionally, I enjoy going outdoors for a relaxing
+                  swim or bike ride.
+                </p>
+              </div>
+            </div>
           </div>
         </section>
         <section id="portfolio">
