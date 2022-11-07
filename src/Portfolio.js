e45eafb86fb6c584339120ead6df0e9270c8babd
Marcin <marcin.p.dev@gmail.com>
2022-11-07 17:18:57 +0100

    forEach iterator in render return

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -15,8 +15,8 @@ class Portfolio extends React.Component {
     fetch("http://localhost:3000/db")
       .then((response) => response.json())
       .then((result) => {
-        console.log("getting result");
-        console.log(result.projects);
+        // console.log("getting result");
+        // console.log(result.projects);
         this.setState({ data: result.projects });
       })
       .catch((err) => console.log("Fetch error" + err));
@@ -27,15 +27,20 @@ class Portfolio extends React.Component {
 
     if (this.state.data !== "") {
       this.state.data.map((project) => {
-        z.push(project.project_name);
+        z.push([project.project_name, project.project_description, project.tech_stack, project.banner]);
+        // console.log(z);
       });
     }
 
-    return <div id="port">
-      <p>{z} | {z.project_description} | {z.tech_stack} | {z.banner}</p>
-      
-      </div>;
-  }
+    return (<div id="port">
+      {console.log(z)}
+      { z.forEach(element => {
+        return `<p> oij ${element.project_name} | ${element.project_description} | ${element.tech_stack} | ${element.banner} </p>`
+      })
+
+      }
+      </div>
+    )}
 }
 
 export default Portfolio;
