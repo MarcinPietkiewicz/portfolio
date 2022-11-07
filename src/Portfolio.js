53fe43693eaea5847bc886a6b1e7d8bd3e8295b1
Marcin <marcin.p.dev@gmail.com>
2022-11-07 16:29:37 +0100

    Getting mock json file via fetch

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -2,8 +2,27 @@ import React from "react";
 import "./Portfolio.scss";
 
 class Portfolio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { projects: "loading..." };
+  }
+
+  async loadData() {
+    fetch("http://localhost:3000/db")
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.projects);
+        let z = data;
+        this.setState({projects: z})
+      });
+  }
+
   render() {
-    return <div id="port">Hello Portfolio</div>;
+    this.loadData();
+    return <div id="port">{this.state.projects}</div>;
   }
 }
 
