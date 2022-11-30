2223b0a436f9fd2693c8c1d42b2d07e07399b29c
Marcin <marcin.p.dev@gmail.com>
2022-11-30 11:43:55 +0100

    Adding filters to portfolio state works now

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "./Portfolio.scss";
 import Card from "./Card";
-import { ThemeProvider } from "react-bootstrap";
 
 class Portfolio extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: "", filters: ['react', 'python'] };
+    this.state = { data: "", filters: ["react"] };
     this.handleFilter = this.handleFilter.bind(this);
   }
 
-handleFilter(e) {
-  let tech = e.target.dataset.tech;
-  let arr = this.state.filters;
-  console.log('button clicked...'+tech);
-  // console.log(arr.length == 0);
-  console.log(arr.some(el => el !== tech));
-  if (arr.some(el => el !== tech)){
-    this.setState({filters: this.state.filters.push(tech)})
+  handleFilter(e) {
+    let tech = e.target.dataset.tech;
+    let arr = this.state.filters;
+    console.log("tech = " + tech + ", this.state.filters = " + this.state.filters + ", arr.length = " + arr.length);
+    // console.log(arr.some(el => el !== tech));
+    if (arr.some((el) => el === tech)) {
+      return this.state;
+    } else {
+      this.setState({ filters: [...this.state.filters, tech] });
+      console.log(this.state.filters);
+    }
+    // this.state.filters.some(tech) ? this.setState({filters: this.state.filters.filter(e => e !== tech)}) : this.setState({filters: [...this.state.filters, tech]});
   }
-  // this.state.filters.some(tech) ? this.setState({filters: this.state.filters.filter(e => e !== tech)}) : this.setState({filters: [...this.state.filters, tech]});
-  console.log(this.state.filters);
-}
 
   componentDidMount() {
     this.fetchProjects();
@@ -32,7 +32,6 @@ handleFilter(e) {
       .then((response) => response.json())
       .then((result) => {
         this.setState({ data: result.projects });
-
       })
       .catch((err) => console.log("Fetch error" + err));
   }
@@ -49,15 +48,33 @@ handleFilter(e) {
 
     return (
       <div id="port">
-        <button className="tags" onClick={this.handleFilter} data-tech="react" id="tag-react">REACT</button>
-        <button className="tags" onClick={this.handleFilter} data-tech="javascript" id="tag-js">Javascript</button>
-        <button className="tags" onClick={this.handleFilter} data-tech="python" id="tag-python">Python</button>
-        <button className="tags" onClick={this.handleFilter} data-tech="PHP" id="tag-php">PHP</button>
-        <button className="tags" onClick={this.handleFilter} data-tech="poc" id="tag-p1">Proof-of-concept</button>
-        <button className="tags" onClick={this.handleFilter} data-tech="simple" id="tag-p2">Simple</button>
-        <button className="tags" onClick={this.handleFilter} data-tech="medium" id="tag-p3">Medium or bigger</button>
-        <button className="tags" onClick={this.handleFilter} data-tech="typescript" id="tag-p3">Typescript</button>
-        <button className="tags" onClick={this.handleFilter} data-tech="redux" id="tag-p3">REDUX</button>
+        <button className="tags" onClick={this.handleFilter} data-tech="react" id="tag-react">
+          REACT
+        </button>
+        <button className="tags" onClick={this.handleFilter} data-tech="javascript" id="tag-js">
+          Javascript
+        </button>
+        <button className="tags" onClick={this.handleFilter} data-tech="python" id="tag-python">
+          Python
+        </button>
+        <button className="tags" onClick={this.handleFilter} data-tech="PHP" id="tag-php">
+          PHP
+        </button>
+        <button className="tags" onClick={this.handleFilter} data-tech="poc" id="tag-p1">
+          Proof-of-concept
+        </button>
+        <button className="tags" onClick={this.handleFilter} data-tech="simple" id="tag-p2">
+          Simple
+        </button>
+        <button className="tags" onClick={this.handleFilter} data-tech="medium" id="tag-p3">
+          Medium or bigger
+        </button>
+        <button className="tags" onClick={this.handleFilter} data-tech="typescript" id="tag-p3">
+          Typescript
+        </button>
+        <button className="tags" onClick={this.handleFilter} data-tech="redux" id="tag-p3">
+          REDUX
+        </button>
         {data.map((element, index) => {
           return <Card key={index} data={element} />;
         })}
