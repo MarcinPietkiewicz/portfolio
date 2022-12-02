0b6b252e69bb1f25cc0e475fe224fb5dde3f7274
Marcin <marcin.p.dev@gmail.com>
2022-12-02 15:59:20 +0100

    CSS for tags part 1

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,24 +7,20 @@ class Portfolio extends React.Component {
     super(props);
     this.state = { data: "", filters: [] };
     this.handleFilter = this.handleFilter.bind(this);
-    this.filterTags = this.filterTags.bind(this);
+    // this.filterTags = this.filterTags.bind(this);
   }
 
   handleFilter(e) {
     if (e.target.dataset.tech === "RESET") {
-      this.setState({filters: []});
+      this.setState({ filters: [] });
       return;
     }
-    console.log(this.state.filters);
     let tech = e.target.dataset.tech;
     let arr = this.state.filters;
-    console.log("tech = " + tech + ", this.state.filters = " + this.state.filters + ", arr.length = " + arr.length);
 
     if (arr.some((el) => el === tech)) {
-      console.log('removing '+tech);
-      this.setState({filters: this.state.filters.filter(el => el !== tech)})
+      this.setState({ filters: this.state.filters.filter((el) => el !== tech) });
     } else {
-      console.log('adding '+tech);
       this.setState({ filters: [...this.state.filters, tech] });
     }
   }
@@ -42,17 +38,13 @@ class Portfolio extends React.Component {
       .catch((err) => console.log("Fetch error" + err));
   }
 
-
   filterTags(tags, data) {
     console.log(tags, data);
-      tags.forEach(tag => {
-        data = data.filter(element => element.tech_stack.some(el => el === tag))
-      }
-      )
-      return data;
-    }
-
-  
+    tags.forEach((tag) => {
+      data = data.filter((element) => element.tech_stack.some((el) => el === tag));
+    });
+    return data;
+  }
 
   render() {
     let data = [];
@@ -66,34 +58,41 @@ class Portfolio extends React.Component {
     }
 
     return (
-      <div id="port">
-        <button className="tags" onClick={this.handleFilter} data-tech="React">
-          REACT
-        </button>
-        <button className="tags" onClick={this.handleFilter} data-tech="Javascript" >
-          Javascript
-        </button>
-        <button className="tags" onClick={this.handleFilter} data-tech="Python" >
-          Python
-        </button>
-        <button className="tags" onClick={this.handleFilter} data-tech="Php" >
-          PHP
-        </button>
-        <button className="tags" onClick={this.handleFilter} data-tech="Typescript">
-          Typescript
-        </button>
-        <button className="tags" onClick={this.handleFilter} data-tech="Redux">
-          REDUX
-        </button>
-        <button className='reset' onClick={this.handleFilter} data-tech="RESET">
-          RESET TAGS
-        </button>
-
-
-        {data.map((element, index) => {
-          return <Card key={index} data={element}/>;
-        })}
-      </div>
+      <React.Fragment>
+        <div id="port-flex">
+          <div id="tags-text">
+          Select one or more project tags
+          </div>
+        <div id="tags">
+          <button className="tag" onClick={this.handleFilter} data-tech="React">
+            REACT
+          </button>
+          <button className="tag" onClick={this.handleFilter} data-tech="Javascript">
+            Javascript
+          </button>
+          <button className="tag" onClick={this.handleFilter} data-tech="Python">
+            Python
+          </button>
+          <button className="tag" onClick={this.handleFilter} data-tech="Php">
+            PHP
+          </button>
+          <button className="tag" onClick={this.handleFilter} data-tech="Typescript">
+            Typescript
+          </button>
+          <button className="tag" onClick={this.handleFilter} data-tech="Redux">
+            REDUX
+          </button>
+          <button className="tag reset" onClick={this.handleFilter} data-tech="RESET">
+            Reset all tags
+          </button>
+          </div>
+        </div>
+        <div id="port">
+          {data.map((element, index) => {
+            return <Card key={index} data={element} />;
+          })}
+        </div>
+      </React.Fragment>
     );
   }
 }
