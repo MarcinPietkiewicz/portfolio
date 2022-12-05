e0aad2bf648e9642a8de7c806bfce83bbc6db74f
Marcin <marcin.p.dev@gmail.com>
2022-12-05 12:14:19 +0100

    Initial second pass card styling and mock json refactor

--- src/Card.js
+++ src/Card.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./Card.scss";
 
 class Card extends React.Component {
-
   render() {
     return (
       <div className="card" key={this.props.data}>
-        <div className="project-title">{this.props.data[1]}</div>
         <div className="project-photo">photo here...</div>
-        <div className="project-description">{this.props.data[2]}</div>
-        <div className="project-techs">{this.props.data[3]}</div>
-        <hr />
-        <div className="project-github">link to github</div>
-        <div className="project-demo">link to demo</div> 
+        <div className="project-hero">
+          <div className="project-title">{this.props.data[1]}</div>
+          <div className="project-description">{this.props.data[2]}</div>
+          <div className="project-techs">{this.props.data[3]}</div>
+          <hr />
+          <div className="project-github">link to github</div>
+          <div className="project-demo">link to demo</div>
+        </div>
       </div>
     );
   }
