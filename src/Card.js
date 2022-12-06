33bb98029c792e4f60c7feaac29e1d47a1b5f7fe
Marcin <marcin.p.dev@gmail.com>
2022-12-06 12:04:54 +0100

    Preliminary work on card css

--- src/Card.js
+++ src/Card.js
@@ -4,18 +4,26 @@ import thumb from "./assets/adam.png";
 
 class Card extends React.Component {
   render() {
+    console.log(this.props.data);
+    let path = `${this.props.data[4]}`;
     return (
-      <div className="card" key={this.props.data}>
+      <div className="card" key={this.props.data[0]}>
         <div className="project-photo">
           <img src={thumb} alt="pic" />
-          </div>
+        </div>
         <div className="project-hero">
           <div className="project-title">{this.props.data[1]}</div>
           <div className="project-description">{this.props.data[2]}</div>
           <div className="project-techs">{this.props.data[3]}</div>
           <hr />
-          <div className="project-github">link to github</div>
-          <div className="project-demo">link to demo</div>
+          <div className="project-links">
+            <div className="project-demo">
+              <a href={this.props.data[4]}>Demo</a>
+            </div>
+            <div className="project-github">
+              <a href={this.props.data[4]}>Code</a>
+            </div>
+          </div>
         </div>
       </div>
     );
