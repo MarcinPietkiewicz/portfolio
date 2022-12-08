7721f8521bb76599426339628b08b85b8918e87d
Marcin <marcin.p.dev@gmail.com>
2022-12-08 14:56:18 +0100

    Centered links at the bottom

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Card.scss";
-import DemoLogo from './assets/demo.svg';
-import CodeLogo from './assets/code.svg';
+import DemoLogo from "./assets/demo.svg";
+import CodeLogo from "./assets/code.svg";
 
 class Card extends React.Component {
   render() {
     console.log(this.props.data);
-
-    
 
     let image_path = this.props.data[4];
     return (
@@ -18,15 +16,24 @@ class Card extends React.Component {
         <div className="project-hero">
           <div className="project-title">{this.props.data[1]}</div>
           <div className="project-description">{this.props.data[2]}</div>
-          <div className="project-techs">{this.props.data[3].map((tag,index) => <li key={index}>{tag}</li>)}</div>
-          <hr />
-          <div className="project-links">
-            <div className="project-demo">
-              <a href={this.props.data[5]} target="_blank" rel="noreferrer"><img src={DemoLogo} height="50px"/></a>
-
-            </div>
-            <div className="project-github">
-              <a href={this.props.data[6]} target="_blank" rel="noreferrer"><img src={CodeLogo} height="50px"/></a>
+          <div className="project-techs">
+            {this.props.data[3].map((tag, index) => (
+              <li key={index}>{tag}</li>
+            ))}
+          </div>
+          <div className="project-footer">
+            <div className="horizontal-bar"></div>
+            <div className="project-links">
+              <div className="project-demo">
+                <a href={this.props.data[5]} target="_blank" rel="noreferrer">
+                  <img src={DemoLogo} alt="demo-logo" height="50px" />
+                </a>
+              </div>
+              <div className="project-github">
+                <a href={this.props.data[6]} target="_blank" rel="noreferrer">
+                  <img src={CodeLogo} alt="code-logo" height="50px" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
