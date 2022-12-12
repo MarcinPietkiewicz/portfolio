2aa908baa9e97b7ad2bcd8a94c94414224cbe668
Marcin <marcin.p.dev@gmail.com>
2022-12-12 15:46:01 +0100

    Added codewars section in contact view

--- src/Codewars.js
+++ src/Codewars.js
@@ -3,21 +3,36 @@ import React from "react";
 class Codewars extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: "data is empty" };
+    this.state = { data: null };
   }
 
-  componentDidMount;
+  componentDidMount() {
+    this.fetchCodewarsData();
+  }
 
   fetchCodewarsData = async () => {
-    const data = await fetch("");
-    const movies = await data.json();
-    this.setState("data: movies");
+    const response = await fetch("https://www.codewars.com/api/v1/users/MarcinPietkiewicz");
+    const data = await response.json();
+    this.setState({ data: data });
   };
 
   render() {
+    let codewars = null;
+    let loadingNote = "fetching data...";
+    if (this.state.data) {
+      codewars = this.state.data;
+    }
+
     return (
       <React.Fragment>
-
+        <div className="codewars-sign">codewars</div>
+      <div className="codewars-element">
+        <p>Codewars honor: <span className="bold">{codewars ? codewars.honor : loadingNote}</span></p>
+        <p>Code challenges completed: <span className="bold">{codewars ? codewars.codeChallenges.totalCompleted : loadingNote}</span></p>
+        <p>Codewars javascript rank: <span className="bold">{codewars ? codewars.ranks.languages.javascript.name : loadingNote}</span></p>
+        <p>Codewars python rank: <span className="bold">{codewars ? codewars.ranks.languages.python.name : loadingNote}</span></p>
+        <p>Overall rank: <span className="bold">{codewars ? codewars.ranks.overall.name : loadingNote}</span></p>
+      </div>
       </React.Fragment>
     );
   }
